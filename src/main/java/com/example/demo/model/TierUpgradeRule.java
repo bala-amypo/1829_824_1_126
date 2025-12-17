cfb70e59ce83d4228022b7636688d3d6049dff3f
bala-amypo <bala@amypo.in>
2025-12-17 09:07:02 +0000

    User commit

--- src/main/java/com/example/demo/model/TierUpgradeRule.java
+++ src/main/java/com/example/demo/model/TierUpgradeRule.java
@@ -2,4 +2,8 @@ package.com.example.demo.model;
 public class TierUpgradeRule{
     private long id;
     private String fromTier;
+    private String toTier;
+    private Double minSpend;
+    private Integer minVisits;
+    private Boolean active;
 }
--- src/main/java/com/example/demo/model/VisitRecord.java
+++ src/main/java/com/example/demo/model/VisitRecord.java
@@ -1,4 +1,7 @@
 package.com.example.demo.model;
 public class VisitRecord{
-    
+    private Long id;
+    private Long customerId;
+    private LocalDate visitDate;
+    private String channel;
 }
