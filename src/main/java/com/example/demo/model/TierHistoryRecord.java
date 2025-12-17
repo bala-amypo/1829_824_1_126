a944e58543af73d42f05957fe16711aead782c31
bala-amypo <bala@amypo.in>
2025-12-17 09:02:22 +0000

    User commit

--- src/main/java/com/example/demo/model/TierHistoryRecord.java
+++ src/main/java/com/example/demo/model/TierHistoryRecord.java
@@ -1,4 +1,9 @@
 package.com.example.demo.model;
 public class TierHistoryRecord{
-    
+    private Long id;
+    private Long customerId;
+    private String oldTier;
+    private String newTier;
+    private String reason;
+    private LocalDateTime changedAt;
 }
--- src/main/java/com/example/demo/model/TierUpgradeRule.java
+++ src/main/java/com/example/demo/model/TierUpgradeRule.java
@@ -1,4 +1,5 @@
 package.com.example.demo.model;
 public class TierUpgradeRule{
-    
+    private long id;
+    private String fromTier;
 }
