34981bccfc583dc108ea731057e6ca2508cdf0dd
bala-amypo <bala@amypo.in>
2025-12-17 08:56:27 +0000

    User commit

--- src/main/java/com/example/demo/model/CustomerProfile.java
+++ src/main/java/com/example/demo/model/CustomerProfile.java
@@ -1,1 +1,11 @@
-package.com.example.
+package.com.example.demo.model;
+public class CustomerProfile{
+    private long id;
+    private String customerid;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String currentTier;
+    private Boolean active;
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -0,0 +1,8 @@
+package.com.example.demo.model;
+public class PurchaseRecord{
+    private long id;
+    private long customerId;
+    private Double amount;
+    private LocalDate purchaseDate;
+    private String storeLocation;
+}
